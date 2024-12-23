422a8685aa3be7107ce944abee436b46d14e8368
Alejandro Castellanos <k2jac9@users.noreply.github.com>
2024-12-22 23:15:40 -0500

    assignment1

--- 02_activities/assignments/assignment1.sql
+++ 02_activities/assignments/assignment1.sql
@@ -1,11 +1,9 @@
 /* ASSIGNMENT 1 */
 /* SECTION 2 */
 
-
 --SELECT
 /* 1. Write a query that returns everything in the customer table. */
 SELECT * FROM customer;
-
 
 /* 2. Write a query that displays all of the columns and 10 rows from the cus- tomer table, 
 sorted by customer_last_name, then customer_first_ name. */
@@ -14,7 +12,6 @@ FROM customer
 ORDER BY customer_last_name, customer_first_name
 LIMIT 10;
 
-
 --WHERE
 /* 1. Write a query that returns all customer purchases of product IDs 4 and 9. */
 -- option 1
@@ -22,12 +19,10 @@ SELECT *
 FROM customer_purchases
 WHERE product_id = 4 OR product_id = 9;
 
-
 -- option 2
 SELECT *
 FROM customer_purchases
 WHERE product_id IN (4, 9);
-
 
 /*2. Write a query that returns all customer purchases and a new calculated column 'price' (quantity * cost_to_customer_per_qty), 
 filtered by vendor IDs between 8 and 10 (inclusive) using either:
@@ -46,7 +41,6 @@ SELECT *,
 FROM customer_purchases
 WHERE vendor_id BETWEEN 8 AND 10;
 
-
 --CASE
 /* 1. Products can be sold by the individual unit or by bulk measures like lbs. or oz. 
 Using the product table, write a query that outputs the product_id and product_name
@@ -59,7 +53,6 @@ SELECT product_id,
            ELSE 'bulk'
        END AS prod_qty_type_condensed
 FROM product;
-
 
 /* 2. We want to flag all of the different types of pepper products that are sold at the market. 
 add a column to the previous query called pepper_flag that outputs a 1 if the product_name 
@@ -76,7 +69,6 @@ SELECT product_id,
        END AS pepper_flag
 FROM product;
 
-
 --JOIN
 /* 1. Write a query that INNER JOINs the vendor table to the vendor_booth_assignments table on the 
 vendor_id field they both have in common, and sorts the result by vendor_name, then market_date. */
@@ -84,7 +76,6 @@ SELECT v.*, vba.*
 FROM vendor v
 INNER JOIN vendor_booth_assignments vba ON v.vendor_id = vba.vendor_id
 ORDER BY v.vendor_name, vba.market_date;
-
 
 /* SECTION 3 */
 -- AGGREGATE
@@ -94,7 +85,6 @@ SELECT vendor_id,
        COUNT(*) AS booth_rent_count
 FROM vendor_booth_assignments
 GROUP BY vendor_id;
-
 
 /* 2. The Farmer’s Market Customer Appreciation Committee wants to give a bumper 
 sticker to everyone who has ever spent more than $2000 at the market. Write a query that generates a list 
@@ -110,7 +100,6 @@ INNER JOIN customer_purchases cp ON c.customer_id = cp.customer_id
 GROUP BY c.customer_id, c.customer_first_name, c.customer_last_name
 HAVING SUM(cp.quantity * cp.cost_to_customer_per_qty) > 2000
 ORDER BY c.customer_last_name, c.customer_first_name;
-
 
 --Temp Table
 /* 1. Insert the original vendor table into a temp.new_vendor and then add a 10th vendor: 
@@ -129,7 +118,6 @@ SELECT * FROM vendor;
 INSERT INTO new_vendor (vendor_id, vendor_name, vendor_type, vendor_owner_first_name, vendor_owner_last_name)
 VALUES (10, 'Thomass Superfood Store', 'Fresh Focused', 'Thomas', 'Rosenthal');
 
-
 -- Date
 /*1. Get the customer_id, month, and year (in separate columns) of every purchase in the customer_purchases table.
 
@@ -139,7 +127,6 @@ SELECT customer_id,
        STRFTIME('%m', market_date) AS month,
        STRFTIME('%Y', market_date) AS year
 FROM customer_purchases;
-
 
 /* 2. Using the previous query as a base, determine how much money each customer spent in April 2022. 
 Remember that money spent is quantity*cost_to_customer_per_qty. 
@@ -151,4 +138,3 @@ SELECT cp.customer_id,
 FROM customer_purchases cp
 WHERE STRFTIME('%m', cp.market_date) = '04' AND STRFTIME('%Y', cp.market_date) = '2022'
 GROUP BY cp.customer_id;
-
